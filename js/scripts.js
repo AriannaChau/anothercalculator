fd59ba765904e0abe827fa0dbab2fa206825c852
Marilyn Carlin and Steven Galvin <marilyn@>
2017-02-01 19:28:33 -0800

    add javascript file with addition function.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+   return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Pick any number:"));
+var number2 = parseInt(prompt("Pick any other number:"));
+
+alert(add(number1, number2));
