df7eb7b5e83e60dc51a7678f9f0ff620a7883e13
Marilyn Carlin and Steven Galvin <steven@>
2017-02-01 19:50:51 -0800

    add division function.

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,12 @@ var number1 = parseInt(prompt("Choose a number:"));
 var number2 = parseInt(prompt("Choose a number to multiply by the first:"));
 
 alert(multiply(number1, number2));
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Choose a number to divide the first number by:"));
+
+alert(divide(number1, number2));
