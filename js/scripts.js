141633a92c064c007b34f7674bd2030fc5f5d789
Arianna Chau and Steven Galvin <arianna@>
2017-02-22 18:58:06 -0800

    Change repetitive code to branching with if..else statements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Business or BackEnd logic
+// Business or Back End logic
 
 var add = function(number1, number2) {
    return number1 + number2;
@@ -16,35 +16,48 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-//Everything from here down is the UI or FrontEnd logic
+//Everything from here down is the UI or Front End logic
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
-    var result = (add(number1, number2));
-    $("#output-add").text(result);
+  $("form#calculator").submit(function(event) {
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    console.log("1st number: " + number1);
+    console.log("2nd number: " + number2);
+    console.log("operator: " + operator);
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
     event.preventDefault();
   });
-  $("form#subtract").submit(function(event) {
-    var number1 = parseInt($("#subtract1").val());
-    var number2 = parseInt($("#subtract2").val());
-    var result = (subtract(number1, number2));
-    $("#output-subtract").text(result);
-    event.preventDefault();
-  });
-  $("form#multiply").submit(function(event) {
-    var number1 = parseInt($("#multiply1").val());
-    var number2 = parseInt($("#multiply2").val());
-    var result = (multiply(number1, number2));
-    $("#output-multiply").text(result);
-    event.preventDefault();
-  });
-  $("form#divide").submit(function(event) {
-    var number1 = parseInt($("#divide1").val());
-    var number2 = parseInt($("#divide2").val());
-    var result = (divide(number1, number2));
-    $("#output-divide").text(result);
-    event.preventDefault();
-  });
+  // $("form#subtract").submit(function(event) {
+  //   var number1 = parseInt($("#subtract1").val());
+  //   var number2 = parseInt($("#subtract2").val());
+  //   var result = (subtract(number1, number2));
+  //   $("#output-subtract").text(result);
+  //   event.preventDefault();
+  // });
+  // $("form#multiply").submit(function(event) {
+  //   var number1 = parseInt($("#multiply1").val());
+  //   var number2 = parseInt($("#multiply2").val());
+  //   var result = (multiply(number1, number2));
+  //   $("#output-multiply").text(result);
+  //   event.preventDefault();
+  // });
+  // $("form#divide").submit(function(event) {
+  //   var number1 = parseInt($("#divide1").val());
+  //   var number2 = parseInt($("#divide2").val());
+  //   var result = (divide(number1, number2));
+  //   $("#output-divide").text(result);
+  //   event.preventDefault();
+  // });
 });
